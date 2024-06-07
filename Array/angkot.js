7e9ada4872a66e5ebc2bccab4f97eb58bfdb9d52
Muhammad Naufal Luthfi <naufaldvf123@gmail.com>
2024-06-07 20:58:27 +0700

    Latihan Function & Array

---
+++ Array/angkot.js
@@ -0,0 +1,41 @@
+// Array Penumpang
+var penumpang = [];
+
+// Function Penumpang Naik
+// Paramater function 1. namaPenumpang & 2. arrayPenumpang
+var tambahPenumpang = function (namaPenumpang, penumpang) {
+  // jika angkot kosong,
+  if (penumpang.length == 0) {
+    // penumpang akan mengisi Array/kursi pertama,
+    penumpang.push(namaPenumpang);
+    // KEembalikan array/return isi penumpang dari function
+    return penumpang;
+  } else {
+    // telusuri seluruh kursi dari awal
+    for (var i = 0; i < penumpang.length; i++) {
+      // jika terdapat array/kursi kosong (penumpang turun)
+      if (penumpang[i] == undefined) {
+        // penumpang selanjutnya duduk/mengisi array yang kosong.
+        penumpang[i] = namaPenumpang;
+
+        // kembalikan isi & dan keluar dari function
+        return penumpang;
+        // Jika sudah ada nama yang sama
+      } else if (penumpang[i] == namaPenumpang) {
+        //tampilkan pesan kesalahannya
+        console.log(namaPenumpang + "Sudah ada di dalam angkot");
+      } else if (i == penumpang.length - 1) {
+        penumpang.push(namaPenumpang);
+      }
+    }
+  }
+};
+
+var ulang = true;
+
+while (ulang) {
+  alert("Halo mau naik angkot mba/mas?");
+  var namaPenumpang = prompt("Masukan Nama Anda!");
+  penumpang = tambahPenumpang(namaPenumpang, penumpang);
+  ulang = confirm("Lagi?");
+}
--- Array/manipulasiArray.js
+++ Array/manipulasiArray.js
@@ -1,32 +1,34 @@
 // Manipulasi Array
 
 // 1. Menambahkan isi Array
-var arr = [];
-arr[0] = "Muhammad";
-arr[1] = "Naufal";
-arr[2] = "Luthfi";
-arr[3] = "Ardian";
+// var arr = [];
+// arr[0] = "Muhammad";
+// arr[1] = "Naufal";
+// arr[2] = "Luthfi";
+// arr[3] = "Ardian";
 
-console.log(arr);
+// console.log(arr);
 
 // Untuk menambahkan isi Array dapat dituliskan nama array dan index-nya, Ex= arr[0] = 'Muhammad'
 
 // Dalam menambahkan isi Array dengan menggunakan cara ini perhatikan urutan index yang dimasukan. jangan sampai terlewat. Apabila terdapat nomor index yang terlewat dan tidak diisi dengan nilai apapun, maka index terus akan berisikan undefined
 
 // 2. Menghapus isi Array
-var arr2 = ["Muhammad", "Naufal", "Luthfi"];
-arr2[1] = undefined;
 
-console.log(arr2[1]);
+// var arr2 = ["Muhammad", "Naufal", "Luthfi"];
+// arr2[1] = undefined;
+
+// console.log(arr2[1]);
 
 // dalam menghapus Array secara manual, nilai yang terdapat didalam array bisa diubah/ditimpa dengan nilai undefined.
 
 // 3. Menampilkan isi Array dengan Looping
-var arr3 = ["Muhammad", "Naufal", "Luthfi"];
 
-for (let i = 0; i < arr3.length; i++) {
-  console.log("Nama Siswa ke-" + (i + 1) + " " + arr3[i]);
-}
+// var arr3 = ["Muhammad", "Naufal", "Luthfi"];
+
+// for (let i = 0; i < arr3.length; i++) {
+//   console.log("Nama Siswa ke-" + (i + 1) + " " + arr3[i]);
+// }
 
 // 4. Methode pada Array
 
@@ -34,7 +36,7 @@ for (let i = 0; i < arr3.length; i++) {
 
 // a) Join
 // Method Join berfungsi untuk menggabungkan seluruh array lalu kemudian mengubah array itu menjadi "String"
-var hari = ["Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu", "Minggu"];
+// var hari = ["Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu", "Minggu"];
 
 // console.log(hari.join(" - "));
 
@@ -46,20 +48,89 @@ var hari = ["Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu", "Minggu"];
 
 // Push merupakan method yang berfungsi untuk menambahkan elemen baru pada akhir array.
 
-hari.push("ahad");
+// hari.push("ahad");
 
 // Pop merupakan method yang berfungsi untuk menghapuskan elemen terakhir pada array;
 
-hari.pop();
+// hari.pop();
 
 // c) unshift & shift
 
 // unshift merupakan method yang digunakan untuk menambahkan elemen baru pada awal array. dalam menambhakan elemen baru pada awal elemen dapat diisi melalui parameter
 
-hari.unshift("Hari");
+// hari.unshift("Hari");
 
 // Shift merupakan method yang digunakan untuk menghilangkan/menghapus elemen pertama pada array. kebalikan dari pop
 
-hari.shift();
+// hari.shift();
 
 // console.log(hari.join(" - "));
+
+// var hari = ["Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu", "Minggu"];
+
+// d) Splice & Slice
+
+// Splice merupakan method yang digunakan untuk menambahkan elemen baru ditengah array.
+
+// syntaks splice yaitu array.splice(indexAwal, jmlElemenSetelahYangAkanDihapus, elemenbaru1, elemenBaru2,.....)
+
+// hari.splice(2, 2, "Ahad");
+
+// console.log(hari);
+
+// var angka = [1, 2, 3, 4, 5];
+// angka.splice(2, 1, 7, 8);
+// console.log(angka);
+
+// Slice merupakan method yang digunakan untuk mengambil/memotong array untuk dipanggil/dijadikan array baru.
+
+// syntaks penulisan slice yaitu array.slice(indexAwal, indexAkhir)
+
+// var angkaSlice = angka.slice(2, 4);
+// console.log(angkaSlice.join("-"));
+
+// e) forEach & Map
+
+// forEach merupakan method yang digunakan untuk melakukan looping pada array, sehingga tidak memerlukan metode looping menggunakan for atau while.
+
+// Syntaks penulisan forEach adalah array.forEach (function () {})
+
+// var nama = ["Naufal", "Luthfi", "Agus"];
+
+// nama.forEach(function (e, i) {
+//   console.log("Nama Mahasiswa ke- " + (i + 1) + " adalah: " + e);
+// });
+
+// parameter 'e' mengacu pada elemen dalam array.
+// parameter 'i' mengacu pada nomor index dalam array/
+
+// Map merupakan method yang digunakan untuk mengembalikan atau return nilai pada array ke dalam array baru.
+
+// syntaks penulisan array.map adalah array.map(functioin() {})
+
+// var angka = [1, 2, 3, 4, 5, 6];
+// var angka2 = angka.map(function (e) {
+//   return e * 2;
+// });
+
+// console.log(angka2.join(" - "));
+
+// f) Sort
+
+// Sort merupakan method yang digunakan untuk mengurutkan elemen pada array.
+// syntaks penulisan array.sort adalah array.sort(function(a, b) {return a - b})
+
+var angka = [4, 5, 1, 2, 6, 8, 3, 7, 9, 20, 10, 15, 30];
+
+// angka.sort(function (a, b) {
+//   return a-b;
+// });
+
+// a-b = akan mengurutkan array dari kecil ke besar (ascending)
+// b-a = akan mengurutkan array dari besar ke kecil (descending)
+
+angka.sort(function (a, b) {
+  return b - a;
+});
+
+console.log(angka.join(" - "));
